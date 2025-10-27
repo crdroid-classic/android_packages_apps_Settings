1bd29c0d2b665ddf9d78bd1563b52bd30320b1cd
Ivy <78825510+Lost-Entrepreneur439@users.noreply.github.com>
2025-10-27 01:02:52 -0230

    Revert "Set class name when createConfirmDeviceCredentialIntent() API is used."
    This reverts commit 5a8e0ffe3eaa773490758b194f7779f9de4143c6.

--- src/com/android/settings/password/BiometricFragment.java
+++ src/com/android/settings/password/BiometricFragment.java
@@ -140,7 +140,6 @@ public class BiometricFragment extends InstrumentedFragment {
                 .setDisallowBiometricsIfPolicyExists(
                         promptInfo.isDisallowBiometricsIfPolicyExists())
                 .setReceiveSystemEvents(true)
-                .setClassNameIfItIsConfirmDeviceCredentialActivity()
                 .build();
     }
 
